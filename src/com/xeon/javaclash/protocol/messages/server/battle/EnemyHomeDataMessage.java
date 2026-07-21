858d6ac4cf261cc1fd40f4cf17690c83976b239b
Astral <132731805+astralsc@users.noreply.github.com>
2026-07-21 09:37:33 +0800

    correct id

--- src/com/xeon/javaclash/protocol/messages/server/battle/EnemyHomeDataMessage.java
+++ src/com/xeon/javaclash/protocol/messages/server/battle/EnemyHomeDataMessage.java
@@ -6,7 +6,7 @@ import com.xeon.javaclash.protocol.messages.PiranhaMessage;
 public class EnemyHomeDataMessage extends PiranhaMessage {
     public EnemyHomeDataMessage(Connection connection){
         super(connection);
-        this.id = 25863;
+        this.id = 21940;
     }
 
     @Override
